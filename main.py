fc088738620cd35f51d97dc68eec045ecee5e38c
arjun <arjunvinodappu@gmail.com>
2026-03-07 16:13:04 +0530

    Updating initial models

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class Supplier(Base):
     __tablename__="suppliers"
     id=Column(Integer, primary_key=True, index=True)
     name=Column(String, nullable=False)
-    content=Column(Text, nullable=False)
-    parts=relationship("Parts", back_populates="supplier")
+    description=Column(Text, nullable=False)
+    parts=relationship("Part", back_populates="supplier")
     rating=Column(Float, default=0.00)
     location=Column(String, nullable=False)
 
@@ -18,6 +18,11 @@ class Part(Base):
     __tablename__="parts"
     id=Column(Integer, primary_key=True, index=True)
     name=Column(String, nullable=False)
+    description=Column(Text)
+    price=Column(Float, nullable=False)
+    car_model=Column(String, nullable=False)
+    stock=Column(Integer, default=0)
+
     supplier_id=Column(Integer, ForeignKey("suppliers.id"))
     supplier=relationship("Supplier", back_populates="parts")
 
@@ -26,11 +31,10 @@ class User(Base):
     __tablename__="users"
     id=Column(Integer, primary_key=True, index=True)
     name=Column(String, nullable=False)
-    age=Column(Integer, nullable=False)
-    email=Column(String, nullable=False)
+    email=Column(String, unique=True, nullable=False)
     password=Column(String, nullable=False)
     role=Column(String, default="Customer")
-    orders=relationship("Orders", back_populates="user")
+    orders=relationship("Order", back_populates="user")
 
 class Order(Base):
     __tablename__="orders"
@@ -38,6 +42,22 @@ class Order(Base):
     status=Column(String, default="Pending")
     user_id=Column(Integer, ForeignKey("users.id"))
     user=relationship("User", back_populates="orders")
+    items=relationship("OrderItem", back_populates="order")
+
+class OrderItem(Base):
+    __tablename__="order_items"
+    id=Column(Integer, primary_key=True, index=True)
+    order_id=Column(Integer, ForeignKey("orders.id"))
+    parts_id=Column(Integer, ForeignKey("parts.id"))
+    order=relationship("Order",back_populates="items")
+
+    
+
+class Complaint(Base):
+    __tablename__="complaints"
+    id=Column(Integer, primary_key=True, index=True)
+    user_id=Column(Integer, ForeignKey("users.id"))
+    status=Column(String, default="Unresolved")
 
 
 app=FastAPI()
